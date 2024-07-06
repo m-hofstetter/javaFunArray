02555e244d5f1fd256566a8f21109523a458da23
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2024-07-06 14:59:49 +0200

    Add new condition

---
+++ src/main/java/program/conditions/ArrayElementEqualToConstant.java
@@ -0,0 +1,21 @@
+package program.conditions;
+
+import base.infint.InfInt;
+import base.interval.Interval;
+import funarray.Expression;
+
+public class ArrayElementEqualToConstant extends ArrayElementInequality {
+  private final InfInt equalConstant;
+
+  public ArrayElementEqualToConstant(InfInt equalConstant, Expression index) {
+    this.equalConstant = equalConstant;
+    this.index = index;
+    this.constraintsIf = Interval.of(equalConstant, equalConstant);
+    this.constraintsElse = Interval.unknown();
+  }
+
+  @Override
+  public String toString() {
+    return String.format("A[%s] == %s", index, equalConstant);
+  }
+}
