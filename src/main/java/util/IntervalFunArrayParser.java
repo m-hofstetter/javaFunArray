81add06061dbb826e6a37768bfb1df725e6f2a7c
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2024-07-17 18:10:22 +0200

    Introduce FunArray parser

---
+++ src/main/java/util/IntervalFunArrayParser.java
@@ -0,0 +1,102 @@
+package util;
+
+import base.infint.*;
+import base.interval.Interval;
+import funarray.Bound;
+import funarray.Expression;
+import funarray.FunArray;
+import funarray.VariableReference;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class IntervalFunArrayParser extends FunArrayBaseVisitor {
+  @Override
+  public FiniteInteger visitFiniteInteger(FunArrayParser.FiniteIntegerContext ctx) {
+    return new FiniteInteger(Integer.parseInt(ctx.getText()));
+  }
+
+  @Override
+  public Infinity visitInfinity(FunArrayParser.InfinityContext ctx) {
+    if (ctx.POS_INF() != null) {
+      return new PositiveInfinity();
+    }
+    if (ctx.NEG_INF() != null) {
+      return new NegativeInfinity();
+    }
+    return null;
+  }
+
+  @Override
+  public InfInt visitInteger(FunArrayParser.IntegerContext ctx) {
+    if (ctx.finiteInteger() != null) {
+      return visitFiniteInteger(ctx.finiteInteger());
+    }
+    if (ctx.infinity() != null) {
+      return visitInfinity(ctx.infinity());
+    }
+    return null;
+  }
+
+  @Override
+  public Interval visitInterval(FunArrayParser.IntervalContext ctx) {
+    return Interval.of(visitInteger(ctx.integer(0)), visitInteger(ctx.integer(1)));
+  }
+
+  @Override
+  public Expression visitExpression(FunArrayParser.ExpressionContext ctx) {
+    var variable = new VariableReference(ctx.variableName().getText());
+    if (ctx.finiteInteger() != null) {
+      return new Expression(variable, visitFiniteInteger(ctx.finiteInteger()));
+    }
+    return new Expression(variable);
+  }
+
+  @Override
+  public Bound visitBound(FunArrayParser.BoundContext ctx) {
+    var expressions = ctx.expression().stream()
+            .map(this::visitExpression)
+            .collect(Collectors.toSet());
+    return new Bound(expressions);
+  }
+
+  @Override
+  public Boolean visitEmptiness(FunArrayParser.EmptinessContext ctx) {
+    return ctx.getText().equals("}?");
+  }
+
+  @Override
+  public FunArray<Interval> visitFunArray(FunArrayParser.FunArrayContext ctx) {
+
+    List<FunArrayParser.BoundContext> bs = ctx.bound();
+
+    var test = ctx.getText();
+
+    List<Bound> bounds = ctx
+            .bound().stream()
+            .map(this::visitBound)
+            .toList();
+
+    var intervals = ctx.interval().stream().map(this::visitInterval).toList();
+    var emptiness = ctx.emptiness().stream().map(this::visitEmptiness).toList();
+    return new FunArray<>(bounds, intervals, emptiness);
+  }
+
+  @Override
+  public FunArray<Interval> visit(ParseTree tree) {
+    return (FunArray<Interval>) super.visit(tree);
+  }
+
+  public static FunArray<Interval> parse(String s) {
+    FunArrayLexer lexer = new FunArrayLexer(CharStreams.fromString(s));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    FunArrayParser parser = new FunArrayParser(tokens);
+    ParseTree tree = parser.funArray();
+    IntervalFunArrayParser visitor = new IntervalFunArrayParser();
+    return visitor.visit(tree);
+  }
+
+
+}
