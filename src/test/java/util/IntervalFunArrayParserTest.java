1ce5b06f0ebc5b7715302e250110f038d03faa11
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2024-07-17 18:15:14 +0200

    Add parser test

---
+++ src/test/java/util/IntervalFunArrayParserTest.java
@@ -0,0 +1,31 @@
+package util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import base.interval.Interval;
+import funarray.Bound;
+import funarray.Expression;
+import funarray.FunArray;
+import funarray.VariableReference;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+public class IntervalFunArrayParserTest {
+
+  @Test
+  void test() {
+    var f = IntervalFunArrayParser.parse("{a} [0, 0] {b} [0, 0] {c};");
+    var comparand = new FunArray<Interval>(
+            List.of(
+                    new Bound(new Expression(new VariableReference("a"))),
+                    new Bound(new Expression(new VariableReference("b"))),
+                    new Bound(new Expression(new VariableReference("c")))
+            ), List.of(
+            Interval.of(0), Interval.of(0)
+    ), List.of(
+            false, false
+    )
+    );
+    assertThat(f).isEqualTo(comparand);
+  }
+}
