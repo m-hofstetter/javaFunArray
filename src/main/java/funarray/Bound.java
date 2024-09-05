ac970ed92eb256db0c2bf2d8f5d29aa1c2b2c83b
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2024-09-05 16:39:23 +0200

    Refactor normal expression

--- src/main/java/funarray/Bound.java
+++ src/main/java/funarray/Bound.java
@@ -20,22 +20,6 @@ public record Bound(Set<NormalExpression> expressions) {
     this(Set.of(expression));
   }
 
-  /**
-   * If a variable v changes reversibly to f(v), all occurrences of v in an FunArray have to be
-   * replaced by f^-1(v), so that it still represents the Array correctly. See Cousot et al. 2011.
-   * For adding to a variable v by an amount i, this means replacing alle occurrences of v by v-1.
-   *
-   * @param varRef the variable v.
-   * @param value  the value by which it is being increased.
-   * @return the altered bound.
-   */
-  public Bound addToVariableInFunArray(String varRef, int value) {
-    return new Bound(
-            expressions.stream()
-                    .map(e -> e.addToVariableInFunArray(varRef, value))
-                    .collect(Collectors.toSet())
-    );
-  }
 
   public Bound adaptForChangedVariableValue(
           String changedVariableRef,
--- src/main/java/funarray/NormalExpression.java
+++ src/main/java/funarray/NormalExpression.java
@@ -15,33 +15,6 @@ public record NormalExpression(String varRef, int constant) {
     this(variable, 0);
   }
 
-  @Override
-  public String toString() {
-    if (constant == 0) {
-      return varRef;
-    }
-    if (varRef.equals("0")) {
-      return String.valueOf(constant);
-    }
-    return "%s%s%s".formatted(varRef, constant < 0 ? "" : "+", constant);
-  }
-
-  /**
-   * If a variable v changes reversibly to f(v), all occurrences of v in an FunArray have to be
-   * replaced by f^-1(v), so that it still represents the Array correctly. See Cousot et al. 2011.
-   * For adding to a variable v by an amount i, this means replacing alle occurrences of v by v-1.
-   *
-   * @param otherVarRef the variable v.
-   * @param value       the value by which it is being increased.
-   * @return the altered variable.
-   */
-  public NormalExpression addToVariableInFunArray(String otherVarRef, int value) {
-    if (this.varRef().equals(otherVarRef)) {
-      return new NormalExpression(otherVarRef, constant - value);
-    }
-    return this;
-  }
-
   public NormalExpression increase(int value) {
     return new NormalExpression(varRef, constant + value);
   }
@@ -50,16 +23,9 @@ public record NormalExpression(String varRef, int constant) {
    * Syntactically compares two expressions.
    *
    * @param other the expression to compare this to.
-   * @return TRUE if this is definitely less than the other, FALSE if this is definitely not less
+   * @return TRUE if this is definitely less equal than the other, FALSE if this is definitely not less
    *         than the other and UNKNOWN if it can't be determined
    */
-  public boolean isLessThan(NormalExpression other) {
-    if (!this.varRef.equals(other.varRef)) {
-      return false;
-    }
-    return this.constant() < other.constant();
-  }
-
   public boolean isLessEqualThan(NormalExpression other) {
     if (!this.varRef.equals(other.varRef)) {
       return false;
@@ -71,16 +37,9 @@ public record NormalExpression(String varRef, int constant) {
    * Syntactically compares two expressions.
    *
    * @param other the expression to compare this to.
-   * @return TRUE if this is definitely greater than the other, FALSE if this is definitely not
+   * @return TRUE if this is definitely greater equal than the other, FALSE if this is definitely not
    *         greater than the other and UNKNOWN if it can't be determined
    */
-  public boolean isGreaterThan(NormalExpression other) {
-    if (!this.varRef.equals(other.varRef)) {
-      return false;
-    }
-    return this.constant() > other.constant();
-  }
-
   public boolean isGreaterEqualThan(NormalExpression other) {
     if (!this.varRef.equals(other.varRef)) {
       return false;
@@ -93,7 +52,13 @@ public record NormalExpression(String varRef, int constant) {
   }
 
   @Override
-  public int hashCode() {
-    return (varRef + constant).hashCode();
+  public String toString() {
+    if (constant == 0) {
+      return varRef;
+    }
+    if (varRef.equals("0")) {
+      return String.valueOf(constant);
+    }
+    return "%s%s%s".formatted(varRef, constant < 0 ? "" : "+", constant);
   }
 }
